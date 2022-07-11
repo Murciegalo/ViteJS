330990ec5dd8d9ec8608838ffb1971ce633db6e8
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-07-11 10:28:31 +0200

    squareComp. in progress

--- tictactoe/tictactoe/src/Game/Game.tsx
+++ tictactoe/tictactoe/src/Game/Game.tsx
@@ -4,13 +4,16 @@ import { INITIAL_GAME_STATE } from '../utils/tempData.js';
 
 const Game = () => {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+  const handleCellClick = (e: any) => {
+    console.log('clicked', e.target.getAttribute('data-cell-index'));
+  };
   return (
     <main>
       <h1>Game</h1>
       <div className='main'>
         <div className='board'>
-          {gameState.map((el: string, key: string) => (
-            <Square key={key}>{el}</Square>
+          {gameState.map((player, I) => (
+            <Square key={I} {...{ player, I }} handleClick={handleCellClick} />
           ))}
         </div>
       </div>
--- tictactoe/tictactoe/src/Square/Square.tsx
+++ tictactoe/tictactoe/src/Square/Square.tsx
@@ -1,17 +1,18 @@
-import { ReactNode } from 'react';
-
 type Props = {
-  player: ReactNode;
+  I: string;
+  player?: string;
+  handleClick(e: any): void;
 };
 
-const Square = ({ player }: Props) => {
+const Square = ({ player, I, handleClick }: Props) => {
+  console.log('DATA');
   // const scale = player ? 'scale-100' : 'scale-0';
   // const textColor = player === 'X' ? 'text-yellow-200' : 'text-fuchsia-300';
   // const hoverStyle = "transition duration-500 hover:scale-105 transform"
 
   return (
-    <div className='listItem'>
-      <span>{player}</span>{' '}
+    <div className='listItem' data-cell-index={I} onClick={handleClick}>
+      <span data-cell-index={I}>{player}</span>{' '}
     </div>
   );
 };
